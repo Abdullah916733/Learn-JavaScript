a89a7ae97d2d4dd5c0f976085e947142b1f03ed0
Abdullah Ansari <abdullah.3mindsdigital@gmail.com>
2024-03-01 17:13:37 +0530

    first commit

---
+++ test.js
@@ -0,0 +1,46 @@
+// remove repeated value in the array.
+// const a = [1, 1, 2, 2, 3, 4];
+// const b = [];
+
+// for (let i = 0; i < a.length; i++) {
+//   let isDuplicate = false;
+//   for (let j = 0; j < b.length; j++) {
+//     if (a[i] === b[j]) {
+//       isDuplicate = true;
+//       break;
+//     }
+//   }
+//   if (!isDuplicate) {
+//     b.push(a[i]);
+//   }
+// }
+
+// console.log(b);
+
+// ---------------------------------------------------------------------------------------------- //
+
+// reverse the array
+
+// const a = [1, 2, 3, 4];
+// const b = [];
+
+// for (let i = a.length - 1; i >= 0; i--) {
+//   b.push(a[i]);
+// }
+
+// console.log(b);
+
+// ---------------------------------------------------------------------------------------------- //
+
+// show value in below format in console
+// 3
+// 6
+// 9
+// 12
+
+// for (let i = 1; i <= 4; i++) {
+//   console.log(i * 3);
+// }
+
+// ---------------------------------------------------------------------------------------------- //
+
